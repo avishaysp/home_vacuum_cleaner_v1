b36f9f0c4794f24c7db63fb550e8c6e256157bd2
avishays-tau <157728644+avishays-tau@users.noreply.github.com>
2024-07-07 21:32:53 +0300

    rename input files

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,19 @@
 #include "io_handling.h"
 #include "VacuumCleaner.h"
 
-int main() {
-    FileReader fr("INPUT_FORMAT2.txt");
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "one argument required - inputfile, got " << argc << std::endl;
+        std::exit(EXIT_FAILURE);
+    }
+    std::string input_file_path = argv[1];
+    std::string output_file_path = "output_" + input_file_path;
+    FileReader fr(input_file_path);
     FileReader::file_reader_output args = fr.readFile();
-    FileWriter m("input_mat.txt");
-    m.writeHouse(args.house_map);
     VacuumCleaner vacuumCleaner(args.max_num_of_steps, args.max_battery_steps, args.house_map, args.docking_loc);
 
     vacuumCleaner.cleanHouse();
-    FileWriter fw("output.txt");
+    FileWriter fw(output_file_path);
     fw.writeHouse(vacuumCleaner.getHouse());
     fw.writePath(vacuumCleaner.getPath());
     return 0;
